5eb921d737cccd903a6f31d85b139ee50376a713
Alessandro Tieri <tieri.alessandro00@gmail.com>
2024-03-20 16:14:57 +0100

    clean

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -4,3 +4,10 @@
     php artisan migrate:fresh
     php artisan db:seed
 @endtask
+
+@task('clean', ['on' => ['localhost']])
+    sudo php artisan ro:cl
+    sudo php artisan ca:cl
+    sudo php artisan co:cl
+    sudo php artisan vi:cl
+@endtask
